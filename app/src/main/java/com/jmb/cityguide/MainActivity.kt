9e26b33fc8c80d763bc2f6c9e04a512a5962e6fd
Jonathan Meriño <jmerino1204@gmail.com>
2021-02-01 20:08:32 -0500

    Dashboard Finish

---
+++ app/src/main/java/com/jmb/cityguide/MainActivity.kt
@@ -0,0 +1,11 @@
+ package com.jmb.cityguide
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
